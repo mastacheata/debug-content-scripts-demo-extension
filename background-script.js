10c98f521032e41feacf118b90f4c9cd6cfb01c5
Benedikt Bauer <mastacheata@gmail.com>
2017-11-20 19:36:55 +0100

    Demo Extension for debugging (inline) content scripts in Firefox Web Extensions

---
+++ background-script.js
@@ -0,0 +1,15 @@
+function onCreated(tab) {
+	debugger;
+	browser.tabs.executeScript(tab.id, 'console.log("Running Content Script from debug-content-scripts-demo-extension"); debugger; console.log("Finished running Content Script from debug-content-scripts-demo-extension");');
+}
+
+function onError(error) {
+	console.error(`Error: ${error}`);
+}
+
+browser.browserAction.onClicked.addListener(() => {
+	var creating = browser.tabs.create({
+		url:"https://example.org"
+	});
+	creating.then(onCreated, onError);
+});
