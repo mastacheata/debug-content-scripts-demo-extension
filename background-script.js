e32e09a0f2a37fc8d26b4f5660fa48224f306ceb
Benedikt Bauer <benedikt@xenzilla.de>
2019-02-07 21:09:51 +0100

    Fix this so it works in Firefox 66.0b5 Apparently the call structure for executeScript has changed and we need a new permission for background Scripts.

--- background-script.js
+++ background-script.js
@@ -1,6 +1,6 @@
 function onCreated(tab) {
 	debugger;
-	browser.tabs.executeScript(tab.id, 'console.log("Running Content Script from debug-content-scripts-demo-extension"); debugger; console.log("Finished running Content Script from debug-content-scripts-demo-extension");');
+	browser.tabs.executeScript(tab.id, {code: 'console.log("Running Content Script from debug-content-scripts-demo-extension"); debugger; console.log("Finished running Content Script from debug-content-scripts-demo-extension");'});
 }
 
 function onError(error) {
